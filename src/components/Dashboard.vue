3a1db96817cba9e06b91a02d1a43b4a34c98a4f0
fredomontero <christian.montero@uabc.edu.mx>
2021-07-07 16:01:23 -0700

    Add Dashboard structure

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dashboard-container">
+    <h1>Shopping List</h1>
+    <Input/>
+    <ItemList/>
+  </div>
+</template>
+
+<script>
+
+import Input from './Input.vue';
+import ItemList from "./ItemList.vue";
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Input,
+    ItemList
+  }
+}
+</script>
+
+<style scoped>
+  .dashboard-container {
+    font-family: 'Quicksand', sans-serif;
+  }
+</style>
